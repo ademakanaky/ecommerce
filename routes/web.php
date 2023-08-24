a26cc7d2ef8dfaef05bc1e16a2f46db92387532c
Oluwasegun Ibidokun <segunibidokun@gmail.com>
2023-08-24 15:50:16 +0100

    fix: remove phpinfo

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    //return view('welcome');
-    return phpinfo();
+    return view('welcome');
 });
 
 Route::middleware(['auth'])->group(function () {
