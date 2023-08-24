a379240d6efb6342dc3c22c88bdb281972e747a4
Oluwasegun Ibidokun <segunibidokun@gmail.com>
2023-08-24 15:48:50 +0100

    feat: Add Command Scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,11 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('kafka:consume-cart-items-topic')->everyMinute()
+            ->withoutOverlapping()->runInBackground();
+
+        $schedule->command('kafka:consume-checkout-items-topic')->everyMinute()
+            ->withoutOverlapping()->runInBackground();
     }
 
     /**
